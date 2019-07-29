eaae9124bd5f5830c41464575e871f32c9617502
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2019-07-29 17:05:07 +0530

    added vuex

--- src/components/HerosTable.vue
+++ src/components/HerosTable.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <input v-model="searchName" />
-    <div v-if="heros.isLoading">
+    <div v-if="isLoading">
       Loading...
     </div>
-    <div v-else-if="heros.isError">
+    <div v-else-if="isError">
       Error in fetching heros
     </div>
     <table v-else>
@@ -18,7 +18,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="hero in heros.filterData" v-bind:key="hero.id">
+        <tr v-for="hero in heros" v-bind:key="hero.id">
           <td><img :src="`http://cdn.dota2.com/${hero.img}`" /></td>
           <td>{{ hero.localized_name }}</td>
           <td>{{ hero.pro_win }}</td>
@@ -31,21 +31,14 @@
 </template>
 
 <script>
-import axios from "axios";
 import debounce from "lodash.debounce";
+import { mapState } from "vuex";
 
 export default {
   name: "HerosTable",
 
   data: function() {
     return {
-      heros: {
-        isLoading: false,
-        isError: false,
-        data: [],
-        filterData: []
-      },
-
       searchName: null
     };
   },
@@ -56,39 +49,21 @@ export default {
     }
   },
 
-  methods: {
-    search: debounce((search, vm) => {
-      const self = vm;
-      self.filterHeros(self);
-    }, 500),
+  computed: mapState({
+    heros: state => state.heros,
+    isLoading: state => state.isLoading,
+    isError: state => state.isError
+  }),
 
-    filterHeros() {
-      const filterData = this.heros.data.filter(hero => {
-        const localizedNameLowerCase = hero.localized_name.toLowerCase();
-        const searchNameLowerCase = this.searchName.toLowerCase();
-        return localizedNameLowerCase.includes(searchNameLowerCase);
-      });
-      this.heros.filterData = filterData;
-    }
+  methods: {
+    search: debounce((search, self) => {
+      const filterData = self.$store.getters.filterHerosByName(search);
+      console.log(filterData.length);
+    }, 500)
   },
 
-  async mounted() {
-    this.heros.isLoading = true;
-    this.heros.isError = false;
-
-    try {
-      const response = await axios.get(
-        "https://api.opendota.com/api/heroStats"
-      );
-      if (response.status === 200) {
-        this.heros.data = response.data;
-        this.heros.filterData = response.data;
-      }
-    } catch (error) {
-      this.heros.isError = true;
-    } finally {
-      this.heros.isLoading = false;
-    }
+  mounted() {
+    this.$store.dispatch("fetchHeros");
   }
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -1,10 +1,56 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    isLoading: false,
+    isError: false,
+    heros: []
+  },
+
+  mutations: {
+    SET_LOADING_STATUS(state, isLoading) {
+      state.isLoading = isLoading;
+    },
+    SET_ERROR_STATUS(state, isError) {
+      state.isError = isError;
+    },
+    SET_HEROS(state, heros) {
+      state.heros = heros;
+    }
+  },
+
+  actions: {
+    async fetchHeros(context) {
+      context.commit("SET_LOADING_STATUS", true);
+      context.commit("SET_ERROR_STATUS", false);
+
+      try {
+        const response = await axios.get(
+          "https://api.opendota.com/api/heroStats"
+        );
+        if (response.status === 200) {
+          context.commit("SET_HEROS", response.data);
+        }
+      } catch (error) {
+        context.commit("SET_ERROR_STATUS", true);
+      } finally {
+        context.commit("SET_LOADING_STATUS", false);
+      }
+    }
+  },
+
+  getters: {
+    filterHerosByName: state => searchName => {
+      console.log(searchName);
+      return state.heros.filter(hero => {
+        const name = hero.localized_name.toLowerCase();
+        const search = searchName.toLowerCase();
+        return name.includes(search);
+      });
+    }
+  }
 });
