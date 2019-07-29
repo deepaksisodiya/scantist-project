a3713573a237416984493e83ccbe1e02823369aa
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2019-07-29 18:05:22 +0530

    added element ui

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="app">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
     <HerosTable />
   </div>
 </template>
