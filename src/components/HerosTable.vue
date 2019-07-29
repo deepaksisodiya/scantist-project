2cdc59182cf617aebd0098c30ce6965cea797d1f
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2019-07-29 15:44:04 +0530

    filtering data

--- src/components/HerosTable.vue
+++ src/components/HerosTable.vue
@@ -1,34 +1,38 @@
 <template>
-  <div v-if="heros.isLoading">
-    Loading...
+  <div>
+    <input v-model="searchName" />
+    <div v-if="heros.isLoading">
+      Loading...
+    </div>
+    <div v-else-if="heros.isError">
+      Error in fetching heros
+    </div>
+    <table v-else>
+      <thead>
+        <tr>
+          <th>Hero Image</th>
+          <th>Hero Name</th>
+          <th>PRO Win Rate</th>
+          <th>Pro Pick Count</th>
+          <th>Pro Ban Count</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="hero in heros.data" v-bind:key="hero.id">
+          <td><img :src="`http://cdn.dota2.com/${hero.img}`" /></td>
+          <td>{{ hero.localized_name }}</td>
+          <td>{{ hero.pro_win }}</td>
+          <td>{{ hero.pro_pick }}</td>
+          <td>{{ hero.pro_ban }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-  <div v-else-if="heros.isError">
-    Error in fetching heros
-  </div>
-  <table v-else>
-    <thead>
-      <tr>
-        <th>Hero Image</th>
-        <th>Hero Name</th>
-        <th>PRO Win Rate</th>
-        <th>Pro Pick Count</th>
-        <th>Pro Ban Count</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="hero in heros.data" v-bind:key="hero.id">
-        <td><img :src="`http://cdn.dota2.com/${hero.img}`" /></td>
-        <td>{{ hero.localized_name }}</td>
-        <td>{{ hero.pro_win }}</td>
-        <td>{{ hero.pro_pick }}</td>
-        <td>{{ hero.pro_ban }}</td>
-      </tr>
-    </tbody>
-  </table>
 </template>
 
 <script>
 import axios from "axios";
+import debounce from "lodash.debounce";
 
 export default {
   name: "HerosTable",
@@ -39,8 +43,28 @@ export default {
         isLoading: false,
         isError: false,
         data: []
-      }
+      },
+      searchName: null
     };
+  },
+
+  watch: {
+    searchName: function() {
+      this.search(this.searchName, this);
+    }
+  },
+
+  methods: {
+    search: debounce((search, vm) => {
+      const self = vm;
+      self.filterHeros(self);
+    }, 500),
+
+    filterHeros() {
+      this.heros.data = this.heros.data.filter(hero => {
+        return hero.localized_name == this.searchName;
+      });
+    }
   },
 
   async mounted() {
